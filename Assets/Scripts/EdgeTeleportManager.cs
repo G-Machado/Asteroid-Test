33c586ecd25830df4a5875237195fb6ddc2ce0e6
Gabriel Machado <gabriel.machado.cg@gmail.com>
2023-03-03 02:20:08 -0300

    Edge teleport

--- Assets/Scripts/EdgeTeleportManager.cs
+++ Assets/Scripts/EdgeTeleportManager.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class EdgeTeleportManager : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.transform.name);
-        Debug.Log(collision.attachedRigidbody.transform.name);
+        if(collision.tag == "topBarrier")
+        {
+            if(rb.velocity.y > 0)
+                transform.position = new Vector3(transform.position.x, transform.position.y * -1, transform.position.z);
+        }
+        else if (collision.tag == "botBarrier")
+        {
+            if(rb.velocity.y < 0)
+                transform.position = new Vector3(transform.position.x, transform.position.y * -1, transform.position.z);
+        }
+        else if (collision.tag == "leftBarrier")
+        {
+            if (rb.velocity.x < 0)
+                transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+        }
+        else if (collision.tag == "rightBarrier")
+        {
+            if (rb.velocity.x > 0)
+                transform.position = new Vector3(transform.position.x * -1, transform.position.y, transform.position.z);
+        }
+
     }
 }
